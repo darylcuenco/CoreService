8992c1c19b1e8ff1f0b0b272b73f4a6a8527a607
DJ <darylcuenco@yahoo.com>
2023-04-25 13:42:22 +0800

    add debug condition

--- Program.cs
+++ Program.cs
@@ -17,8 +17,12 @@ builder.Services.AddSwaggerGen();
 
 var configuration = builder.Configuration;
 int port = configuration.GetValue<int>("PORT");
-string path = configuration.GetValue<string>("CERT_PATH");
 string key = configuration.GetValue<string>("CERT_KEY");
+#if DEBUG
+string path = @"..\..\..\..\certs\my-cert.pfx";
+#else
+string path = configuration.GetValue<string>("CERT_PATH");
+#endif
 Console.WriteLine("base path: " + Path.Combine(AppContext.BaseDirectory, path) + " port: " + port + " cert path: " + path + " cert key: " + key);
 builder.WebHost.UseKestrel(options =>
 {
